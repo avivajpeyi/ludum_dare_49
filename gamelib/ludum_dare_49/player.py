f73066d0870d29d567f302f6a6434ad1d800650c
Reinhold Willcox <reinhold.willcox@mail.mcgill.ca>
2021-10-02 22:02:05 +1000

    added broken player and laser code - needs to ported over to the new pymunk

--- gamelib/ludum_dare_49/player.py
+++ gamelib/ludum_dare_49/player.py
@@ -1,166 +1,94 @@
-import sys
+import numpy as np
 
-import numpy
 import pygame
+import pymunk
 
-pygame.init()
+from ludum_dare_49 import colors
+from .game_object import GameObject
 
-# Wrap everything in a try except loop
-try:
+### Setup static objects
 
-    ### Setup static objects
+# Set up draw window
 
-    # Set up draw window
-    SCREEN_SIZE = 1000
-    screen = pygame.display.set_mode([SCREEN_SIZE, SCREEN_SIZE])
+### Create the triangle character here
+class Player(GameObject):
+    def __init__(self):
+        """
+        Initialize the player, which is a rotating triangle.
+        Set the initial color, shape, and orientation.
+        """
+        super(Player, self).__init__()
+        self.color = colors.RED
+        self.theta = 0
+        self.rotation_speed = 0.1
+        self.center = (SCREEN_SIZE / 2.0, SCREEN_SIZE / 2.0)
+        self.aspect_ratio = 3  # height / width of isosceles triangle
+        self.scale = SCREEN_SIZE / 10.0  # how big to make it
+        self.relative_corners = self.get_relative_corners()
 
-    ### Create the triangle character here
-    class Player(pygame.sprite.Sprite):
-        def __init__(self):
-            """
-            Initialize the player, which is a rotating triangle.
-            Set the initial color, shape, and orientation.
-            """
-            super(Player, self).__init__()
-            self.color = (255, 0, 0)
-            self.theta = 0
-            self.rotation_speed = 0.1
-            self.center = (SCREEN_SIZE / 2.0, SCREEN_SIZE / 2.0)
-            self.aspect_ratio = 3  # height / width of isosceles triangle
-            self.scale = SCREEN_SIZE / 10.0  # how big to make it
-            self.relative_corners = self.get_relative_corners()
+    def _init_rigid_body(self)
+        rigid_body = pymunk.Body(body_type=pymunk.Body.STATIC)
 
-        def get_relative_corners(self):
-            """
-            Calculate the initial corner coordinates from the aspect ratio and scale
-            """
-            aspect_ratio = self.aspect_ratio
-            scale = self.scale
-            unscaled_coords = [(-1, 0), (1, 0), (0, aspect_ratio)]
-            relative_corners = [
-                [scale * coord[0], scale * coord[1]]
-                for coord in unscaled_coords
-            ]
-            return relative_corners
+    def get_relative_corners(self):
+        """
+        Calculate the initial corner coordinates from the aspect ratio and scale
+        """
+        aspect_ratio = self.aspect_ratio
+        scale = self.scale
+        unscaled_coords = [(-1, 0), (1, 0), (0, aspect_ratio)]
+        relative_corners = [
+            [scale * coord[0], scale * coord[1]]
+            for coord in unscaled_coords
+        ]
+        return relative_corners
 
-        def draw(self):
-            """
-            Function to draw (or redraw) the triangle
-            """
-            coordinates = [
-                [sum(coords) for coords in zip(self.center, corner)]
-                for corner in self.relative_corners
-            ]
-            pygame.draw.polygon(screen, self.color, coordinates, 0)
-            print(self.color)
-            print(coordinates)
+    def draw(self):
+        """
+        Function to draw (or redraw) the triangle
+        """
+        coordinates = [
+            [sum(coords) for coords in zip(self.center, corner)]
+            for corner in self.relative_corners
+        ]
+        pygame.draw.polygon(screen, self.color, coordinates, 0)
+        print(self.color)
+        print(coordinates)
 
-        def update(self, pressed_keys):
-            """
-            Update the player state depending on which keys are pressed
-            """
+    def update(self, pressed_keys):
+        """
+        Update the player state depending on which keys are pressed
+        """
 
-            # If left or right arrow pressed, apply rotation
-            if pressed_keys[pygame.K_LEFT] | pressed_keys[pygame.K_RIGHT]:
-                self.rotate(pressed_keys)
+        # If left or right arrow pressed, apply rotation
+        if pressed_keys[pygame.K_LEFT] | pressed_keys[pygame.K_RIGHT]:
+            self.rotate(pressed_keys)
 
-            # If spacebar pressed, fire laser
-            if pressed_keys[pygame.K_SPACE]:
-                self.fire_laser()
+        # If spacebar pressed, fire laser
+        if pressed_keys[pygame.K_SPACE]:
+            self.fire_laser()
 
-            self.draw()
+        self.draw()
 
-        def rotate(self, pressed_keys):
-            """
-            Rotate the triangle a fixed amount
-            """
-            # Calculate change in theta
-            if pressed_keys[pygame.K_RIGHT]:
-                dtheta = self.rotation_speed
-            if pressed_keys[pygame.K_LEFT]:
-                dtheta = -self.rotation_speed
+    def rotate(self, pressed_keys):
+        """
+        Rotate the triangle a fixed amount
+        """
+        # Calculate change in theta
+        if pressed_keys[pygame.K_RIGHT]:
+            dtheta = self.rotation_speed
+        if pressed_keys[pygame.K_LEFT]:
+            dtheta = -self.rotation_speed
 
-            # Update current angle
-            self.theta += dtheta
-            self.relative_corners = [
-                pygame.math.Vector2(p).rotate(dtheta)
-                for p in self.relative_corners
-            ]
+        # Update current angle
+        self.theta += dtheta
+        self.relative_corners = [
+            pygame.math.Vector2(p).rotate(dtheta)
+            for p in self.relative_corners
+        ]
 
-        def fire_laser(self):
-            radius = self.aspect_ratio * self.scale
-            laser = Laser(self.center, radius, self.theta)
-
-    class Laser(pygame.sprite.Sprite):
-        def __init__(self, origin, radius, theta):
-            """
-            Initialize the laser based on the current position of the player
-            """
-            self.color = (255, 255, 0)
-            self.length = 10
-            self.width = 2
-            self.velocity = 1.0
-            self.origin = origin
-            self.radius = radius
-            self.theta = theta
-            self.position = self.get_position()
-            self.fire()
-
-        # def __del__(self):
-
-        def get_position(self):
-            r = self.radius
-            theta = self.theta
-            center = self.origin
-
-            relative_offset = (radius * np.cos(theta), radius * np.sin(theta))
-            return [sum(coords) for coords in zip(center, relative_offset)]
-
-        # def fire(self):
-
-        # def draw(self):
-
-    # Create player Fred and place on screen
-    print("hey!")
-    fred = Player()
-    fred.draw()
-    print("did this work?")
-
-    # Render the entire screen
-    pygame.display.flip()
-
-    # Continue until user quits
-    running = True
-    while running:
-
-        # Check if window should be closed
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_BACKSPACE:
-                    running = False
-
-        # Make it white
-        screen.fill((255, 255, 255))
-
-        # Check for pressed keys and update player position
-        pressed_keys = pygame.key.get_pressed()
-        fred.update(pressed_keys)
-
-        pygame.draw.circle(
-            screen,
-            (0, 0, 0),
-            (SCREEN_SIZE / 2.0, SCREEN_SIZE / 2.0),
-            SCREEN_SIZE / 10.0,
-        )
-        pygame.display.flip()
-
-except Exception as e:
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    print(exc_type, fname, exc_tb.tb_lineno)
-    print(e)
+    def fire_laser(self):
+        radius = self.aspect_ratio * self.scale
+        laser = Laser(self.center, radius, self.theta)
 
 
-pygame.quit()
+
---
+++ test/player_test.py
@@ -0,0 +1,60 @@
+import pygame
+
+from ludum_dare_49 import colors
+from ludum_dare_49.constants import FPS, HEIGHT, WIDTH
+from ludum_dare_49.player import Player
+from ludum_dare_49.physics import GamePhysicsHandler
+from ludum_dare_49.planet import Planet
+
+
+def test_player():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    clock = pygame.time.Clock()
+
+    physics_handler = GamePhysicsHandler(screen, FPS)
+    planet = Planet(
+        size=40,
+        screen=screen,
+        color=colors.YELLOW,
+        physics_handler=physics_handler,
+    )
+    """
+    enemy = Enemy(
+        x=50,
+        y=50,
+        size=5,
+        screen=screen,
+        physics_handler=physics_handler,
+        color=colors.RED,
+    )
+    """
+    player = Player(
+
+    )
+
+    while True:
+        for event in pygame.event.get():
+            if (
+                event.type == pygame.QUIT
+                or event.type == pygame.KEYDOWN
+                and event.key == pygame.K_ESCAPE
+            ):
+                exit()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                pygame.image.save(screen, "planet.png")
+
+        # 'planet' in the center of screen
+        screen.fill(pygame.Color("black"))
+        planet.draw()
+        player.draw()
+        physics_handler.update()
+        pygame.display.flip()
+        clock.tick(FPS)
+
+        pygame.display.set_caption("fps: " + str(clock.get_fps()))
+
+
+if __name__ == "__main__":
+    test_enemy()
